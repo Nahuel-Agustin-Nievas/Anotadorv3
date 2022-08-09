1e0b2408f18c46aa47a95d22adefb822271cc602
tito3333 <nievasnahuel@gmail.com>
2022-08-09 12:43:44 +0200

    LocalStorage

---
+++ app.js
@@ -0,0 +1,16 @@
+document.getElementById("formTask").addEventListener("submit", saveTask);
+
+function saveTask(e) {
+
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("description").value;
+
+
+    const task = {
+        title: title,
+        description: description
+    };
+
+    localStorage.setItem("task", JSON.stringify(task));
+e.preventDefault();
+}
