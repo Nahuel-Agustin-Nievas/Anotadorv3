e6f182fc5b9cd665b3b4f07bdaceaa16ed435961
tito3333 <nievasnahuel@gmail.com>
2022-08-09 17:05:37 +0200

    Anotador v3 front

--- app.js
+++ app.js
@@ -7,10 +7,56 @@ function saveTask(e) {
 
 
     const task = {
-        title: title,
-        description: description
+        title,
+        description
     };
 
-    localStorage.setItem("task", JSON.stringify(task));
+     if (localStorage.getItem("tasks") === null) {
+        let tasks = [];
+        tasks.push(task);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    } else {
+      let tasks = JSON.parse(localStorage.getItem("tasks"));
+      tasks.push(task);   
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+
+    getTask();
+
+    document.getElementById("formTask").reset();
 e.preventDefault();
+
 }
+
+function getTask () {
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    let tasksList = document.getElementById("tasks");
+
+    tasksList.innerHTML = "";
+
+    for (let i = 0; i < tasks.length; i++) {
+        let title = tasks[i].title;
+        let description = tasks[i].description;
+
+        tasksList.innerHTML += `<div class= "card mb-3">
+          <div class="card-body">
+           <p> ${title} - ${description} </p>
+           <a class="btn btn-danger" onclick = "deleteTask('${title}')">Borrar</a>
+          </div>
+        </div>`;
+  }
+}
+
+function deleteTask(title) {
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].title == title) {
+            tasks.splice(i, 1);
+        }
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    getTask();  
+}
+getTask();
